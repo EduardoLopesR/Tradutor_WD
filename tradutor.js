d11da46c40e5920d43172f55881173217dd05aec
Eduardo Lopes <edulopesresende19@gmail.com>
2026-04-27 16:38:56 -0300

    Inclusão do tradutor
    Foi incluido o tradutor ( Não totalmente fiel à fonte ) e adicionou pequenas mudanças as outros arquivos

--- tradutor.js
+++ tradutor.js
@@ -1,5 +1,4 @@
-
-
+// Função para auto redimensionar a textarea
 function autoResize(el) {
     const maxHeight = 120; 
 
@@ -12,4 +11,44 @@ function autoResize(el) {
         el.style.height = el.scrollHeight + "px";
         el.style.overflowY = "hidden"; // sem scroll
     }
+}
+// Local para armazenar o replace das letras
+const mapaWD = {
+  A: "✌", 
+  B: "○", // no lugar de 👌
+  C: "☑", // no lugar de 👍
+  D: "☒", // no lugar de 👎
+  E: "☜",
+  F: "☞",
+  G: "☝",
+  H: "☟",
+  I: "✋",
+  J: "☺",
+  K: "☐", // no lugar de 😐
+  L: "☹",
+  M: "✹", // no lugar de 💣
+  N: "☠",
+  O: "⚐",
+  P: "⚑",
+  Q: "✈",
+  R: "☼",
+  S: "💧",
+  T: "❄",
+  U: "✞",
+  V: "✠",
+  W: "✡",
+  X: "☪",
+  Y: "☯",
+  Z: "☸"
+};
+
+function traduzir() {
+  const input = document.getElementById("texto").value;
+
+  const resultado = input.split("").map(letra => {
+    if (letra === " ") return " ";
+    return mapaWD[letra.toUpperCase()] || letra;
+  }).join("");
+
+  document.getElementById("resultado").value = resultado;
 }
